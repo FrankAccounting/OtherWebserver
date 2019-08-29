5ec60412468f5954bd8c0bc4d4c29b091e24e5fb
Frank <Frank@frank.com>
2019-08-28 21:26:06 -0500

    Initial commit

---
+++ src/Servlet.java
@@ -0,0 +1,21 @@
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "Servlet", urlPatterns = "/dragon")
+public class Servlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String p = request.getParameter("param1");
+
+        String msg = "<html><body>"+p+"</html></body>";
+
+        response.getWriter().print(msg);
+    }
+}
