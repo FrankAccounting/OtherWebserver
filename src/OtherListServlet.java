b2baf3c3be3f0d688cfefdc51482035e19d25326
Frank <Frank@frank.com>
2019-09-16 20:02:12 -0500

    Closed the SQL, RSET, and CONN

--- src/OtherListServlet.java
+++ src/OtherListServlet.java
@@ -1,6 +1,7 @@
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.Statement;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -14,10 +15,10 @@ import java.sql.SQLException;
 
 
 @WebServlet(name = "ListServlet",
-        urlPatterns = "/otherlist")
-public class ListServlet extends HttpServlet {
-    private  final String  USER = "root";
-    private  final String  PW = "root";
+        urlPatterns = "/list")
+public class OtherListServlet extends HttpServlet {
+    private final String  USER = "root";
+    private final String  PW = "root";
     private final String DATABASE_PATH = "/WEB-INF/lib/SavageWorlds";
     private final String DRIVER_NAME = "jdbc:derby:";
 
@@ -27,30 +28,33 @@ public class ListServlet extends HttpServlet {
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         Connection conn = null;
-        PreparedStatement pstmt = null;
         ResultSet rset = null;
+        Statement stmt = null;
 
 
         try {
             Class.forName("org.apache.derby.jdbc.EmbeddedDriver");
             String path = getServletContext().getRealPath(DATABASE_PATH);
             conn = DriverManager.getConnection(DRIVER_NAME + path, USER, PW);
+            //make statment
+            stmt = conn.createStatement();
 
-            //SQL command to get all
-            pstmt = conn.prepareStatement("SELECT * FROM player_character");
-            //RUN command
-            rset = pstmt.executeQuery();
+            rset =stmt.executeQuery("SELECT * FROM player_character");
+
+
+
+
 
             StringBuilder html = new StringBuilder("<html><body>");
 
             //ITR over DB call
             while (rset.next()) {
                 //For each line in responce
+                String name = rset.getString("character_nm");
                 int id = rset.getInt("character_id");
                 String align = rset.getString("align");
                 String desc  = rset.getString("description");
-
-                html.append("<li>").append(id+", ").append(align+", ").append(desc+", ").append("</li>");
+                html.append("<li>").append(name+", ").append(id+", ").append(align+", ").append(desc+", ").append("</li>");
             }
 
             html.append("</body></html>");
@@ -60,7 +64,9 @@ public class ListServlet extends HttpServlet {
         } catch (ClassNotFoundException | SQLException ex) {
             response.getWriter().print(ex.getMessage());
         }
-
+         finally {
+            DatabaseUtils.closeAll(conn,stmt,rset);
+        }
     }
 
 
