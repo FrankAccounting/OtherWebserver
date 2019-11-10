c7a3d32612da3473b6f6952293c5f2dbbbc0fbd8
Frank <Frank@frank.com>
2019-11-10 13:28:37 -0600

    Update and delete

--- src/hibernate/HibernateApp.java
+++ src/hibernate/HibernateApp.java
@@ -34,8 +34,12 @@ public class HibernateApp {
             //newSessionApp.createGear();
             //newSessionApp.createGearStore();
             //newSessionApp.listPlayers();
-            newSessionApp.listGearStore();
-            newSessionApp.listGear();
+            //newSessionApp.listGearStore();
+            //newSessionApp.listGear();       Does not close gracfully for sure
+            //newSessionApp.updateGear();      Breaks :c
+            //newSessionApp.updatePlayers();
+            //newSessionApp.updateGearStore();   //Runs but does not update
+            newSessionApp.deleteGear();
 
         } finally {
             newSessionApp.close();
@@ -107,12 +111,81 @@ public class HibernateApp {
         List<GearStore> allStores = session.createQuery("from GearStore").getResultList();
 
         System.out.print(allStores.iterator());
+        session.getTransaction().commit();
+    }
+
+    private void updatePlayers() {
+        Session session = factory.getCurrentSession();
+        session.beginTransaction();
+
+        PlayerCharacter aPlayerCharacter = session.get(PlayerCharacter.class, 1);
+
+        if (aPlayerCharacter != null)
+            aPlayerCharacter.setDescription(aPlayerCharacter.getDescription() + "it worked");
 
         session.getTransaction().commit();
     }
 
 
+    private void updateGear() {
+        Session session = factory.getCurrentSession();
+        session.beginTransaction();
 
+        Gear aPeiceOfGear = session.get(Gear.class, 1);
+
+        if (aPeiceOfGear != null)
+            aPeiceOfGear.setDescription(aPeiceOfGear.getDescription() + "it worked");
+
+        session.getTransaction().commit();
+    }
+
+    private void updateGearStore() {
+        Session session = factory.getCurrentSession();
+        session.beginTransaction();
+
+        GearStore updateGearStore = session.get(GearStore.class, 1);
+
+        if (updateGearStore != null)
+            updateGearStore.setGearId(1);
+
+        session.getTransaction().commit();
+    }
+
+    private void deletePlayer() {
+        Session session = factory.getCurrentSession();
+        session.beginTransaction();
+
+        PlayerCharacter aPlayerCharacter = session.get(PlayerCharacter.class, 1);
+
+        if (aPlayerCharacter != null)
+            session.delete(aPlayerCharacter);
+
+        session.getTransaction().commit();
+    }
+
+    private void deleteGear() {
+        Session session = factory.getCurrentSession();
+        session.beginTransaction();
+
+        Gear aPeiceOfGear = session.get(Gear.class, 1);
+
+        if (aPeiceOfGear != null)
+            session.delete(aPeiceOfGear);
+
+        session.getTransaction().commit();
+    }
+
+    private void deleteGearStore() {
+        Session session = factory.getCurrentSession();
+        session.beginTransaction();
+
+        GearStore updateGearStore = session.get(GearStore.class, 1);
+
+        if (updateGearStore != null)
+            session.delete(updateGearStore);
+
+        session.getTransaction().commit();
+    }
 
 
     }
