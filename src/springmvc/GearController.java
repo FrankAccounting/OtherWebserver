dcb3aa8e54474f69c7acf5a3bf2e3364130dd907
Frank <Frank@frank.com>
2019-11-25 16:45:40 -0600

    fixed the glaring errors

--- src/springmvc/GearController.java
+++ src/springmvc/GearController.java
@@ -17,7 +17,7 @@ import java.util.List;
 
 @Controller
 @RequestMapping("/gear")
-public class GearController{
+public class GearController {
     @Autowired
     private GearService gearService;
 
@@ -25,16 +25,16 @@ public class GearController{
     public String listDonuts(Model theModel) {
         List<Gear> gearList = gearService.listGear();
 
-        theModel.addAttribute("gearList",gearList);
+        theModel.addAttribute("gearList", gearList);
 
         return "list-gear";
     }
 
     @RequestMapping("/showAddGearForm")
-    public String showAddGearForm(Model theModel){
+    public String showAddGearForm(Model theModel) {
         Gear newGear = new Gear();
 
-        theModel.addAttribute("aGear",newGear);
+        theModel.addAttribute("aGear", newGear);
         return "add-gear-form";
     }
 
@@ -50,10 +50,11 @@ public class GearController{
     }
 
     @RequestMapping("/showUpdateGearForm")
-    public String showUpdategearForm(@RequestParam("gearId")                    int theId,                    Model theModel) {
+    public String showUpdategearForm(@RequestParam("gearId") int theId, Model theModel) {
         Gear theGear = gearService.getGear(theId);
         theModel.addAttribute("agear", theGear);
         return "add-gear-form";
 
 
+    }
 }
